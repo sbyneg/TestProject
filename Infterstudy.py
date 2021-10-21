672cdc573afd59f212ff1d34ab291b41887d8fa8
liwang <sbyneg@163.com>
2021-10-21 10:53:29 +0800

    create

---
+++ Infterstudy.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self,mes):
+        self.assertEqual(100, “结果错误”)
+
+
+if __name__ == '__main__':
+    unittest.main()
